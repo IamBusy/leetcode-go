862b4f444d459441bb1d167ce6e93b34b5c93cc5
william wei <william@williamdeMacBook-Air.local>
2017-02-26 11:30:51 +0800

    finish findMedianSortedArrays

---
+++ MedianofTwoSortedArrays/findMedianSortedArrays.go
@@ -0,0 +1,36 @@
+package MedianofTwoSortedArrays
+
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	length := len(nums1)+len(nums2)
+	len1 := len(nums1)
+	len2 := len(nums2)
+
+	//var merged [length]int
+	var merged = make([]int,length,length)
+	var p1,p2 int =0,0
+	for p1< len1 && p2<len2  {
+		if nums1[p1] < nums2[p2]{
+			merged[p1+p2] = nums1[p1]
+			p1++
+		}else {
+			merged[p1+p2] = nums2[p2]
+			p2++
+		}
+	}
+	for p1<len1  {
+		merged[p1+p2] = nums1[p1]
+		p1++
+	}
+	for p2<len2  {
+		merged[p1+p2] = nums2[p2]
+		p2++
+	}
+
+	if length%2 == 0{
+		return float64(merged[length/2]+merged[length/2-1])/2.0
+	}else{
+		return float64(merged[length/2+1])
+	}
+
+}
---
+++ MedianofTwoSortedArrays/test.go
@@ -0,0 +1,12 @@
+package MedianofTwoSortedArrays
+
+import "fmt"
+
+func main() {
+	//var nums1 []int = []int{1,3}
+	//var nums2 []int = []int{2}
+	var nums1 []int = []int{}
+	var nums2 []int = []int{2,3}
+	fmt.Println(findMedianSortedArrays(nums1,nums2))
+
+}
